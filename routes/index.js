86e99bf522d5f78fad69cc9b4dc1c6e2122f4246
Gabriel da Silva Brito <gabrieldsb95@gmail.com>
2022-07-11 12:22:40 -0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var db = require('../database/database.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+	res.render('index');
 });
 
 //Post task
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
 
 	db.push(
 		{
@@ -16,8 +16,13 @@ router.post('/', function(req, res, next) {
 			taskDone: false
 		}
 	)
-	res.render('index', {db});
-	console.log(db);
+	res.render('index', { db });
+
 });
+
+router.post('/clear', (req, res, next) => {
+	db = [];
+	res.render('index');
+})
 
 module.exports = router;
